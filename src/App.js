ae5baea879b06f1bbd7c2e413e430484732ea171
alexbarros <114031281+alexinventor@users.noreply.github.com>
2023-03-21 14:51:36 -0300

    Update App.js
    No projeto base desenvolvido no Peer Tutoring 1, crie uma nova pasta dentro do src. Nela, elabore um arquivo JavaScript com uma função que retorne uma mensagem ao chamar http://localhost:3000/. Observe o exemplo:
    
    Por fim, evidencie o código e o resultado. Depois, anexe na atividade. Lembre-se de digitar export default + nomedafuncao no final do código para que ele seja acessível em outro código. Por exemplo: export default teste.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <header>
-            <h1>Alex Barros</h1>
-      </header>
+      <Header title="Meu Primeiro Componente" />
     </div>
   );
 }
