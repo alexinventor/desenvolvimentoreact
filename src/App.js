77e8e6de7bc19821638a866267a5181c5cee830f
alexbarros <114031281+alexinventor@users.noreply.github.com>
2023-03-25 19:08:32 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
 
 function App() {
+
+  const [contador, setContador] = useState(0);
+
+  function handleButtonClick() {
+    setContador(contador + 1);
+  }
+
   return (
     <div className="App">
-      <Header title='Estou usando Props!' />
+      <Header title={contador} />
+      <input type="button" value="Clique aqui" onClick={handleButtonClick} />
     </div>
   );
 }
